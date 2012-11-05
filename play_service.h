188e985557025a58318c5fc1cfd29f4b119d4c71
Johns <johns98@gmx.net>
2012-11-05 16:02:58 +0100

    Play 3d service api.

---
+++ play_service.h
@@ -0,0 +1,30 @@
+///
+///	@file play_service.h @brief Play service header file.
+///
+///	Copyright (c) 2012 by durchflieger.  All Rights Reserved.
+///
+///	Contributor(s):
+///
+///	License: AGPLv3
+///
+///	This program is free software: you can redistribute it and/or modify
+///	it under the terms of the GNU Affero General Public License as
+///	published by the Free Software Foundation, either version 3 of the
+///	License.
+///
+///	This program is distributed in the hope that it will be useful,
+///	but WITHOUT ANY WARRANTY; without even the implied warranty of
+///	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///	GNU Affero General Public License for more details.
+///
+///	$Id$
+//////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#define PLAY_OSD_3DMODE_SERVICE	"Play-Osd3DModeService-v1.0"
+
+typedef struct
+{
+    int Mode;
+} Play_Osd3DModeService_v1_0_t;
